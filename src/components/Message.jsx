e3734db92527d6aa651c88388d373f77fcfabca8
JoynerM1426 <migue1409.mg@gmail.com>
2023-01-08 19:50:05 -0500

    change-error-image

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,7 @@ const Message = () => {
   return(
  <div>
   <h1 className="message">Esta bebida esta en proceso, porfavor elija otro Elixir y difrute...</h1>
-  <img className="experiment" src="/src/assets/classroom-scene-with-scientist-doing-experiment-free-vector.jpg" alt="" />
+  <img className="experiment" src="/public/classroom-scene-with-scientist-doing-experiment-free-vector.jpg" alt="" />
  </div>
   )
 
