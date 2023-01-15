559ea36a31989b6466c03269eb76bb21b3ae2454
JoynerM1426 <migue1409.mg@gmail.com>
2023-01-15 17:33:53 -0500

    deleteImage

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,6 @@ const Message = () => {
   return(
  <div>
   <h1 className="message">Esta bebida esta en proceso, porfavor elija otro Elixir y difrute...</h1>
-  <img className="experiment" href="/public/images.jpeg" alt="" />
  </div>
   )
 
