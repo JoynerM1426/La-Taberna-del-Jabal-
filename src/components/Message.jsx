ae4a033da67d7bb051d57fe096d946071432d996
JoynerM1426 <migue1409.mg@gmail.com>
2023-01-15 17:26:42 -0500

    changeImage

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,7 @@ const Message = () => {
   return(
  <div>
   <h1 className="message">Esta bebida esta en proceso, porfavor elija otro Elixir y difrute...</h1>
-  <img className="experiment" src="https://www.google.com/search?q=imagen+de+cient%C3%ADfico&rlz=1C1ALOY_esPA1030PA1030&sxsrf=AJOqlzXWRu8_HBQe0pJ42u2X2-cGUmphnA:1673279503058&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjH347z67r8AhWbfDABHeFeB90Q_AUoAXoECAEQAw&biw=1517&bih=730&dpr=0.9#imgrc=-tbWptphegyNHM" alt="" />
+  <img className="experiment" href="/public/images.jpeg" alt="" />
  </div>
   )
 
